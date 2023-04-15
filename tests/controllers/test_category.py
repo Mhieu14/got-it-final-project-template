73c0f83777b77300e76c8f021d6f748dd339f709
buiminhhieuno5@gmail.com <buiminhhieuno5@gmail.com>
2023-04-16 00:37:49 +0700

    feat: testcase get and delete categories

--- tests/controllers/test_category.py
+++ tests/controllers/test_category.py
@@ -1,24 +1,29 @@
-from tests.utils.init_data import generate_auth_headers
+from tests.utils.init_data import generate_auth_headers, init_category, init_user
 
-default_name = "cate0_email0"
+default_name = "cate0_user0"
 default_description = "Category 0 description"
 
 
 def test_create_category_success(client):
     request_body = {"name": default_name, "description": default_description}
-    headers = generate_auth_headers()
+    users = init_user(1)
+    headers = generate_auth_headers(users[0].id)
     response = client.post("/categories", json=request_body, headers=headers)
     assert response.status_code == 200
     assert "id" in response.json
     assert response.json["name"] == default_name
     assert response.json["description"] == default_description
     assert "user_id" in response.json
-    assert "created_at" in response.json
-    assert "updated_at" in response.json
 
 
-def test_create_category_fail(client):
-    headers = generate_auth_headers()
+def test_create_category_invalid_body(client):
+    users = init_user(1)
+    headers = generate_auth_headers(users[0].id)
+    # missing req body
+    response = client.post("/categories", headers=headers)
+    assert response.status_code == 400
+    assert response.json["error_code"] == 400001
+
     # missing name
     response = client.post("/categories", json={"name": default_name}, headers=headers)
     assert response.status_code == 400
@@ -50,3 +55,117 @@ def test_create_category_fail(client):
     )
     assert response.status_code == 400
     assert response.json["error_code"] == 400001
+
+
+def test_create_category_duplicate_name(client):
+    users = init_user(1)
+    headers = generate_auth_headers(users[0].id)
+    client.post(
+        "/categories",
+        json={"name": default_name, "description": default_description},
+        headers=headers,
+    )
+    response = client.post(
+        "/categories",
+        json={"name": default_name, "description": "a diffirent des"},
+        headers=headers,
+    )
+    assert response.status_code == 400
+    assert response.json["error_code"] == 400000
+
+
+def test_get_list_category_success(client):
+    users = init_user(1)
+    user_id = users[0].id
+    init_category(user_id=user_id, number_of_cate=30)
+    headers = generate_auth_headers(user_id=user_id)
+
+    response = client.get("/categories", headers=headers)
+    assert response.status_code == 200
+    assert "categories" in response.json
+    assert "pagination" in response.json
+    first = response.json["categories"][0]
+    assert "id" in first
+    assert "name" in first
+    assert "description" not in first
+    assert "user_id" in first
+    assert len(response.json["categories"]) == 20
+    assert response.json["pagination"]["total"] == 30
+    assert response.json["pagination"]["offset"] == 0
+    assert response.json["pagination"]["limit"] == 20
+
+    response = client.get("/categories?offset=20", headers=headers)
+    assert len(response.json["categories"]) == 10
+    assert response.json["pagination"]["total"] == 30
+    assert response.json["pagination"]["offset"] == 20
+    assert response.json["pagination"]["limit"] == 20
+
+    response = client.get("/categories?limit=10", headers=headers)
+    assert len(response.json["categories"]) == 10
+    assert response.json["pagination"]["total"] == 30
+    assert response.json["pagination"]["offset"] == 0
+    assert response.json["pagination"]["limit"] == 10
+
+
+def test_get_one_category_success(client):
+    users = init_user(1)
+    user_id = users[0].id
+    categories = init_category(user_id=user_id, number_of_cate=1)
+    category = categories[0]
+    headers = generate_auth_headers(user_id=user_id)
+
+    response = client.get(f"/categories/{category.id}", headers=headers)
+    assert response.status_code == 200
+    assert response.json["id"] == category.id
+    assert response.json["name"] == category.name
+    assert response.json["description"] == category.description
+    assert response.json["user_id"] == category.user_id
+    assert response.json["description"] == category.description
+
+
+def test_get_one_category_notfound(client):
+    users = init_user(1)
+    user_id = users[0].id
+    categories = init_category(user_id=user_id, number_of_cate=1)
+    category = categories[0]
+    headers = generate_auth_headers(user_id=user_id)
+
+    response = client.get(f"/categories/{category.id + 1}", headers=headers)
+    assert response.status_code == 404
+    assert response.json["error_code"] == 404000
+
+
+def test_delete_category_success(client):
+    users = init_user(1)
+    user_id = users[0].id
+    categories = init_category(user_id=user_id, number_of_cate=1)
+    category = categories[0]
+    headers = generate_auth_headers(user_id=user_id)
+
+    response = client.delete(f"/categories/{category.id}", headers=headers)
+    assert response.status_code == 200
+
+
+def test_delete_category_notfound(client):
+    users = init_user(1)
+    user_id = users[0].id
+    categories = init_category(user_id=user_id, number_of_cate=1)
+    category = categories[0]
+    headers = generate_auth_headers(user_id=user_id)
+
+    response = client.delete(f"/categories/{category.id + 1}", headers=headers)
+    assert response.status_code == 404
+    assert response.json["error_code"] == 404000
+
+
+def test_delete_category_forbidden(client):
+    users = init_user(2)
+    user_id_create = users[0].id
+    user_id_delete = users[1].id
+    categories = init_category(user_id=user_id_create, number_of_cate=1)
+    category = categories[0]
+    headers = generate_auth_headers(user_id=user_id_delete)
+
+    response = client.delete(f"/categories/{category.id}", headers=headers)
+    assert response.status_code == 403
+    assert response.json["error_code"] == 403000
--- tests/utils/init_data.py
+++ tests/utils/init_data.py
@@ -2,6 +2,7 @@ from flask_jwt_extended import create_access_token
 
 from main import db
 from main.commons.utils import hash_password
+from main.models.category import CategoryModel
 from main.models.user import UserModel
 
 
@@ -10,7 +11,7 @@ def get_email_by_index(email_index):
 
 
 def get_cate_name_by_index(cate_index, email_index):
-    return f"cate{cate_index}_email{email_index}"
+    return f"cate{cate_index}_user{email_index}"
 
 
 def init_user(number_of_user=2):
@@ -26,27 +27,18 @@ def init_user(number_of_user=2):
     return users
 
 
-def generate_auth_headers(email_index=0):
-    users = init_user(number_of_user=1)
-    token = create_access_token(identity=users[0].id, fresh=True)
+def generate_auth_headers(user_id):
+    token = create_access_token(identity=user_id, fresh=True)
     return {"Authorization": f"Bearer {token}"}
 
 
-def get_user_by_index(email_index=0):
-    user: UserModel = UserModel.query.filter(
-        UserModel.email == f"email{email_index}@email.com"
-    ).first()
-    return user
-
-
-def init_category(email_index=0, number_of_cate=10):
-    user = get_user_by_index(email_index)
+def init_category(user_id, number_of_cate=10):
     cates = []
     for i in range(number_of_cate):
-        cate = UserModel(
-            name=get_cate_name_by_index(i, email_index),
+        cate = CategoryModel(
+            name=get_cate_name_by_index(i, user_id),
             description="Description",
-            user_id=user.id,
+            user_id=user_id,
         )
         db.session.add(cate)
         cates.append(cate)
