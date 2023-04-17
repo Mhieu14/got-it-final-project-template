c714b44f1ce365875bcecee57b5a9e0242fe9e02
MHieu <mhieu@mhieus-mbp.lan>
2023-04-17 17:29:41 +0700

    feat: add testcases for auth

--- tests/controllers/test_auth.py
+++ tests/controllers/test_auth.py
@@ -1,3 +1,6 @@
+from datetime import timedelta
+from time import sleep
+
 from flask_jwt_extended import create_access_token
 
 from main import app
@@ -21,5 +24,16 @@ def test_missing_bearer(client):
     user = init_user(number_of_user=1)[0]
     token = create_access_token(identity=user.id, fresh=True)
     headers = {"Authorization": f"{token}"}
+    response = client.get("/categories", headers=headers)
+    assert response.status_code == 401
+
+
+def test_expired_token(client):
+    user = init_user(number_of_user=1)[0]
+    app.config.update(JWT_ACCESS_TOKEN_EXPIRES=timedelta(seconds=2))
+    headers = generate_auth_headers(user_id=user.id)
+    response = client.get("/categories", headers=headers)
+    assert response.status_code == 200
+    sleep(2)
     response = client.get("/categories", headers=headers)
     assert response.status_code == 401
